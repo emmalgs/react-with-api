c4387c68da660d8934b265a8b0f1c75c76c0d11a
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-17 09:12:17 -0700

    get top stores from NYT
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import '../App.css';
+import TopStories from './TopStories'
+
+function App() {
+  return (
+      <React.Fragment>
+        <TopStories />
+      </React.Fragment>
+  );
+}
+
+export default App;
